968314541bb276c83a895764fcd68a63ce7ab29a
Hunter <hlharris22@gmail.com>
2023-03-03 16:37:53 -0500

    Added weapon draw functionality

--- Weapon.js
+++ Weapon.js
@@ -1,67 +1,108 @@
 class Weapon {
-    constructor(name, defaultAnimation, attackAnimation, card, projectile, projectileCount, projectileAngle, manaCost) {
-        this.name = name;
-        this.card = card;
-        this.defaultAnimation = defaultAnimation;
-        this.activeAnimation = defaultAnimation;
-        this.attackAnimation = attackAnimation;
-        this.projectile = projectile;
-        this.projectileCount = projectileCount;
-        this.projectileAngle = projectileAngle;
-        this.manaCost = manaCost;
-    }
+  constructor(
+    name,
+    defaultAnimation,
+    attackAnimation,
+    card,
+    projectile,
+    projectileCount,
+    projectileAngle,
+    manaCost
+  ) {
+    this.name = name;
+    this.card = card;
+    this.defaultAnimation = defaultAnimation;
+    this.activeAnimation = defaultAnimation;
+    this.attackAnimation = attackAnimation;
+    this.projectile = projectile;
+    this.projectileCount = projectileCount;
+    this.projectileAngle = projectileAngle;
+    this.manaCost = manaCost;
+  }
 
-    switchTo() {
-        this.activeAnimation = this.defaultAnimation;
-        if (!this.activeAnimation.isAnimating())
-            this.activeAnimation.start();
-    }
+  switchTo() {
+    this.activeAnimation = this.defaultAnimation;
+    if (!this.activeAnimation.isAnimating()) this.activeAnimation.start();
+  }
 
-    isReady() {
-        return this.activeAnimation === this.defaultAnimation;
-    }
+  isReady() {
+    return this.activeAnimation === this.defaultAnimation;
+  }
 
-    attack(level, camera, audio) {
-        //only shoot if weapon is ready and ammo is in the magazine
-        if (this.isReady() && camera.playerMana - this.manaCost > 0) {
-            audio.playWeaponAttack(this.name);
-            this.activeAnimation.stop();
-            this.activeAnimation = this.attackAnimation;
-            this.activeAnimation.start();
-            camera.playerMana -= this.manaCost;
-            if (this.projectile !== undefined) {
-                for(let p = 0; p < this.projectileCount; p++) {
-                    let angleModifier = p % 2 == 0 ? p * -1 * this.projectileAngle : p * this.projectileAngle;
-                    level.projectiles.push(this.projectile.copy(camera.x, camera.y, Math.cos(camera.angle + angleModifier), Math.sin(camera.angle + angleModifier), true));
-                }
-                
-            }
+  attack(level, camera, audio) {
+    //only shoot if weapon is ready and ammo is in the magazine
+    if (this.isReady() && camera.playerMana - this.manaCost > 0) {
+      audio.playWeaponAttack(this.name);
+      this.activeAnimation.stop();
+      this.activeAnimation = this.attackAnimation;
+      this.activeAnimation.start();
+      camera.playerMana -= this.manaCost;
+      if (this.projectile !== undefined) {
+        for (let p = 0; p < this.projectileCount; p++) {
+          let angleModifier =
+            p % 2 == 0
+              ? p * -1 * this.projectileAngle
+              : p * this.projectileAngle;
+          level.projectiles.push(
+            this.projectile.copy(
+              camera.x,
+              camera.y,
+              Math.cos(camera.angle + angleModifier),
+              Math.sin(camera.angle + angleModifier),
+              true
+            )
+          );
         }
+      }
+    }
+  }
+
+  stopAttack() {
+    if (this.activeAnimation != this.defaultAnimation)
+      this.activeAnimation.requestEnd();
+  }
+
+  draw(screenBuffer) {
+    if (!this.isReady() && !this.activeAnimation.isAnimating()) {
+      this.activeAnimation = this.defaultAnimation;
+      this.activeAnimation.start();
     }
 
-    stopAttack() {
-        if (this.activeAnimation != this.defaultAnimation)
-        this.activeAnimation.requestEnd();
+    let animationFrameBuffer = this.activeAnimation.getFrameBuffer();
+    for (let i = 0; i < animationFrameBuffer.data.length; i = i + 4) {
+      if (animationFrameBuffer.data[i + 3] != 0) {
+        screenBuffer.data[i] = animationFrameBuffer.data[i];
+        screenBuffer.data[i + 1] = animationFrameBuffer.data[i + 1];
+        screenBuffer.data[i + 2] = animationFrameBuffer.data[i + 2];
+        screenBuffer.data[i + 3] = animationFrameBuffer.data[i + 3];
+      }
     }
+  }
 
-    draw(screenBuffer) {
-        if (!this.isReady() && !this.activeAnimation.isAnimating()) {
-            this.activeAnimation = this.defaultAnimation;
-            this.activeAnimation.start();
-        }
+  drawCard(position, yStart, ctx) {
+    const textOffset = 45;
+    ctx.drawImage(this.card, Math.floor(position), Math.floor(yStart));
+    ctx.fillStyle = 'black';
+    ctx.fillText('Damage', position + textOffset, yStart + 80);
+    ctx.fillText(
+      `${this.projectile.damageType}`,
+      position + textOffset,
+      yStart + 90
+    );
+    ctx.fillText('Mana', position + textOffset, yStart + 100);
+    ctx.fillText('Range', position + textOffset, yStart + 110);
+  }
 
-        let animationFrameBuffer = this.activeAnimation.getFrameBuffer();
-        for (let i = 0; i < animationFrameBuffer.data.length; i = i + 4) {
-            if (animationFrameBuffer.data[i+3] != 0) {
-                screenBuffer.data[i] = animationFrameBuffer.data[i];
-                screenBuffer.data[i+1] = animationFrameBuffer.data[i+1];
-                screenBuffer.data[i+2] = animationFrameBuffer.data[i+2];
-                screenBuffer.data[i+3] = animationFrameBuffer.data[i+3];
-            }
-        }
-    }
-
-    copy(projectile, projectileCount, projectileAngle, manaCost) {
-        return new Weapon(this.name, this.defaultAnimation.copy(), this.attackAnimation.copy(), this.card, projectile, projectileCount, projectileAngle, manaCost);
-    }
+  copy(projectile, projectileCount, projectileAngle, manaCost) {
+    return new Weapon(
+      this.name,
+      this.defaultAnimation.copy(),
+      this.attackAnimation.copy(),
+      this.card,
+      projectile,
+      projectileCount,
+      projectileAngle,
+      manaCost
+    );
+  }
 }
--- WeaponMenu.js
+++ WeaponMenu.js
@@ -3,34 +3,68 @@ class WeaponMenu {
     this.menuBackgroundImage = document.getElementById('menuBackground');
     this.card = document.getElementById('card');
     this.newWeaponSelected = false;
-    this.newWeapons = [2, 3, 3];
+    this.newWeapons = [this.card, this.card, this.card];
     this.ctx = ctx;
+    this.borderWidth = 1;
+    this.borderGrowth = 0.2;
   }
 
   //fix
   drawNewWeaponMenu(currentWeapons) {
     let width = this.ctx.canvas.width;
     this.ctx.drawImage(this.menuBackgroundImage, 0, 0);
-
+    const cardHeight = this.card.height;
     const cardWidth = this.card.width;
     const numNewCards = this.newWeapons.length;
     const numCurrentCards = currentWeapons.length;
 
     // Top row new Weapons
-    this.drawWeaponRow(width, cardWidth, numNewCards, 35);
+    this.drawWeaponRow(width, cardWidth, numNewCards, 35, this.newWeapons);
+    this.drawSelection(width, cardWidth, numNewCards, cardHeight, 35, 20);
 
     // Bottom row current weapons
-    this.drawWeaponRow(width, cardWidth, numCurrentCards, 220);
+    // this.drawWeaponRow(width, cardWidth, numCurrentCards, 220, currentWeapons);
+    // this.drawSelection(width, cardWidth, numCurrentCards, cardHeight, 220, 20);
   }
 
-  drawWeaponRow(canvasWidth, cardWidth, numCards, yStart) {
-    const gap = (canvasWidth - cardWidth * numCards) / (numCards + 1);
+  drawWeaponRow(canvasWidth, cardWidth, numCards, yStart, weapons) {
+    const gap = this.calculateGap(canvasWidth, cardWidth, numCards);
     let position = gap;
-    for (let i = 0; i < numCards; i++) {
-      console.log('Drawing card at', gap, yStart);
-      this.ctx.drawImage(this.card, position, yStart);
+    // for (let i = 0; i < numCards; i++) {
+    //   this.ctx.drawImage(this.card, position, yStart);
+    //   position += gap + cardWidth;
+    // }
+    weapons.forEach((element) => {
+      // this.ctx.drawImage(element.card, position, yStart);
+      element.drawCard(position, yStart, this.ctx);
       position += gap + cardWidth;
+    });
+  }
+
+  calculateGap(canvasWidth, cardWidth, numCards, yStart) {
+    const gap = (canvasWidth - cardWidth * numCards) / (numCards + 1);
+    return gap;
+  }
+
+  drawSelection(canvasWidth, cardWidth, numCards, cardHeight, yStart, padding) {
+    const gap = this.calculateGap(canvasWidth, cardWidth, numCards);
+    const xPos = gap - padding;
+    const yPos = yStart - padding;
+    const borderHeight = cardHeight + padding * 2;
+    const borderLength =
+      gap * (numCards - 1) + cardWidth * numCards + padding * 2;
+
+    if (this.borderWidth >= 5) {
+      this.borderGrowth *= -1;
+    } else if (this.borderWidth <= 0) {
+      this.borderGrowth *= -1;
     }
+    this.borderWidth += this.borderGrowth;
+    this.ctx.beginPath();
+    this.ctx.lineWidth = this.borderWidth;
+    this.ctx.rect(xPos, yPos, borderLength, borderHeight);
+    this.ctx.strokeStyle = '#FFFF00';
+    this.ctx.stroke();
   }
 
   // Handle weapon generation
@@ -38,4 +72,23 @@ class WeaponMenu {
   // Handle Selection highlight
 
   // Handle selection
+
+  generateNewWeapons(numWeapons) {
+    // Add random generation of properties for weapon
+    this.newWeapons = [];
+    for (let i = 0; i < numWeapons; i++) {
+      this.newWeapons.push(
+        new Weapon(
+          'Fists',
+          'defaultAnimation',
+          'attackAnimation',
+          document.getElementById('card'),
+          'projectile',
+          'projectileCount',
+          'projectileAngle',
+          'manaCost'
+        )
+      );
+    }
+  }
 }
