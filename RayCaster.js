5e949803ebd08124d0672cc80ac89ca459069117
HeapOverfl0w <jgregorybest@gmail.com>
2023-03-05 18:03:24 -0500

    Fixed Raycaster to use alpha on walls.

--- RayCaster.js
+++ RayCaster.js
@@ -55,8 +55,6 @@ class RayCaster {
       let floor = (cvsHeight + 32) / 2 * (1 + 1/z)  - 12;
       if (rayData.texture !== undefined && rayData.texture.height > 64)
         wallLength *= 2;
-      if (rayData.texture !== undefined && rayData.texture.height < 64)
-        wallLength /= 2;
       let ceiling = floor - wallLength;
 
       rayData.height = wallLength;
@@ -81,7 +79,7 @@ class RayCaster {
           let textureSample = 4 * (ySample * rayData.texture.width + xSample);
           let screenBufferSample = 4 * ((bufferCeiling + y) * cvsWidth + x);
 
-          if (255 != rayData.texture.data[textureSample+3] + (rayData.distance / this.maxShadeDistance)) {
+          if (0 != rayData.texture.data[textureSample+3]) {
             let r = rayData.texture.data[textureSample] + (rayData.distance / this.maxShadeDistance) * this.shadeColor.r;
             let g = rayData.texture.data[textureSample+1] + (rayData.distance / this.maxShadeDistance) * this.shadeColor.g;
             let b = rayData.texture.data[textureSample+2] + (rayData.distance / this.maxShadeDistance) * this.shadeColor.b;
