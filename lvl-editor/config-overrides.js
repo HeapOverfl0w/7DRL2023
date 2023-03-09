43f42c4a358681cb5b1a197f86f2016471ccf8c5
HeapOverfl0w <jgregorybest@gmail.com>
2023-03-09 18:45:09 -0500

    Removed unused stuff.

--- lvl-editor/config-overrides.js
+++
@@ -1,6 +0,0 @@
-const {aliasDangerous, aliasJest, configPaths} = require('react-app-rewire-alias/lib/aliasDangerous')
-
-const aliasPaths = configPaths('./jsconfig.json')
-
-module.exports = aliasDangerous(aliasPaths)
-module.exports.jest = aliasJest(aliasPaths)
